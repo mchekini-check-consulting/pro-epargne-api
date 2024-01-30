d6ab082b2b196a5cfd16bbde84642e7ed2bc18b1
Amoura-md-rachid <mohand-rachid.amoura@cmconsulting-dz.com>
2024-01-30 17:11:22 +0100

    add secu conf #PE-38

--- src/main/java/com/checkconsulting/proepargne/resources/ApplicationInformationResource.java
+++ src/main/java/com/checkconsulting/proepargne/resources/ApplicationInformationResource.java
@@ -3,6 +3,7 @@ package com.checkconsulting.proepargne.resources;
 
 import com.checkconsulting.proepargne.model.AppInformation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,8 @@ public class ApplicationInformationResource {
         this.appInformation = appInformation;
     }
 
+
+    @PreAuthorize("hasAuthority('EMPLOYEE')")
     @GetMapping
     public AppInformation getApplicationInformations(){
         return AppInformation.builder()
