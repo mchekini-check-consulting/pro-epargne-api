702cb164e1c4a0158875cda955de3a268a06ae06
sskarim85@gmail.com <sskarim85@gmail.com>
2024-02-09 11:28:44 +0100

    added swagger

---
+++ src/main/java/com/checkconsulting/proepargne/config/SwaggerConfiguration.java
@@ -0,0 +1,19 @@
+package com.checkconsulting.proepargne.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Pro epargne API",
+                contact = @Contact(
+                        name = "M. Chekini",
+                        url = "https://github.com/mchekini-check-consulting"
+                ),
+                version = "1"
+        )
+)
+public class SwaggerConfiguration {
+}
