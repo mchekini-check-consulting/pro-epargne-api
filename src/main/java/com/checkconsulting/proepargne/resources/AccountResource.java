55e17d9108ee8e77c6021203688ded9528843201
yacinebenkaidali <fy_benkaidali@esi.dz>
2024-02-09 15:48:12 +0100

    Initial update account implementation

---
+++ src/main/java/com/checkconsulting/proepargne/resources/AccountResource.java
@@ -0,0 +1,30 @@
+package com.checkconsulting.proepargne.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.checkconsulting.proepargne.aspect.authentication.Authenticated;
+import com.checkconsulting.proepargne.dto.account.AccountUpdateDto;
+import com.checkconsulting.proepargne.service.AccountService;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("api/v1/account")
+public class AccountResource {
+
+    private final AccountService accountService;
+
+    public AccountResource(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PutMapping()
+    @Authenticated(authenticated = true)
+    public ResponseEntity<?> updateAccoutn(@RequestBody AccountUpdateDto payload) {
+        var reponse = this.accountService.updateAccount(payload);
+        return ResponseEntity.ok().body(reponse);
+    }
+}
