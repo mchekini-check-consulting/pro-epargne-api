27ae2bebb32f4d8b0b4d692526b0c17f66f53ebc
sskarim85@gmail.com <sskarim85@gmail.com>
2024-02-09 15:07:04 +0100

    set up swagger

--- src/main/java/com/checkconsulting/proepargne/handlers/GlobalResponseHandler.java
+++ src/main/java/com/checkconsulting/proepargne/handlers/GlobalResponseHandler.java
@@ -1,6 +1,4 @@
 package com.checkconsulting.proepargne.handlers;
-
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
@@ -9,10 +7,12 @@ import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+import static com.checkconsulting.proepargne.utils.Constants.GLOBAL_RESPONSE_IGNORE_PATHS;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalResponseHandler implements ResponseBodyAdvice {
+
 
     @Override
     public boolean supports(MethodParameter returnType, Class converterType) {
@@ -28,12 +28,16 @@ public class GlobalResponseHandler implements ResponseBodyAdvice {
             ServerHttpRequest request,
             ServerHttpResponse response
     ) {
+        String requestPath = request.getURI().getPath();
 
         int responseStatus = ((ServletServerHttpResponse) response).getServletResponse().getStatus();
 
-        if (responseStatus >= 200 && responseStatus < 300) {
+        if (responseStatus >= 200 && responseStatus < 300 && !GLOBAL_RESPONSE_IGNORE_PATHS.contains(requestPath) && body != null
+                && !"application/octet-stream".equals(selectedContentType.toString())) {
+
             return ResponseTemplate.builder().data(body).build();
         }
+
         return body;
     }
 }
